129d1434030e99bb62b5f314d7b960154408cd7d
Saeid <saeidmaroufi66@gmail.com>
2017-09-29 21:33:43 +0330

    gas limit=5000

--- core/genesis.go
+++ core/genesis.go
@@ -310,10 +310,10 @@ func GenesisBlockForTesting(db ethdb.Database, addr common.Address, balance *big
 func DefaultGenesisBlock() *Genesis {
 	return &Genesis{
 		Config:     params.MainnetChainConfig,
-		Nonce:      0x214652414e4b4f21,
+		Nonce:      66,
 		ExtraData:  hexutil.MustDecode("0x4672616e6b6f497346726565646f6d"),
-		GasLimit:   0x1388,
-		Difficulty: big.NewInt(107374),
+		GasLimit:   5000,
+		Difficulty: big.NewInt(1073741),
 	//	Mixhash:   "0x0000000000000000000000000000000000000000000000000000000000000000",
 		Alloc:      decodePrealloc(mainnetAllocData),
 	}
